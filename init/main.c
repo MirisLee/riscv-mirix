fc8b2c09f1182e48fdcb6581400e2eeec5c84a29
MirisLee <88961599+MirisLee@users.noreply.github.com>
2022-02-10 15:46:08 +0800

    maic.c

---
+++ init/main.c
@@ -0,0 +1,24 @@
+/* init/main.c */
+#include <asm.h>
+#include <kernel.h>
+
+void main() {
+    if (r_mhartid() != 0) {
+        schedule();
+    }
+    con_init();
+    print_init();
+    printk("riscv-mirix kernel booting ...\n\n");
+    kalloc_init();
+    kpage_init();
+    proc_init();
+    trap_init();
+    plic_init();
+    buf_init();
+    inode_init();
+    file_init();
+    disk_init();
+    user_init();
+    __sync_synchronize();
+    schedule();
+}
